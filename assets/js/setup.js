bc769922e3505ae3fb0bb818ba288dcc95ce573c
hasiful <hasifulislam32@gmail.com>
2023-12-21 02:07:31 +0600

    shop

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -1,6 +1,6 @@
 (function ($) {
     $(document).ready(function() {
-        $('.xzoom, .xzoom-gallery').xzoom({zoomWidth: 400, title: true, tint: '#333', Xoffset: 15});
+        $('.xzoom, .xzoom-gallery').xzoom({ zoomWidth: 300, zoomHeight: 200, title: true, tint: '#333', Xoffset: 15});
 
         //Integration with hammer.js
         var isTouchSupported = 'ontouchstart' in window;
